7691863408b802e3ce4fc75e6a5c6dfde4de507a
Ghostintheshell <111326458+Ghostintheshell-iuseArchbtw@users.noreply.github.com>
2025-08-20 14:23:06 -0700

    Fix newline output in C driver

--- kernel/driver.c
+++ kernel/driver.c
@@ -2,4 +2,12 @@
 
 void kernel_driver_hello() {
     printf("Kernel driver: Hello from C!\n");
+    fflush(stdout);
 }
+
+int kernel_driver_add(int a, int b) {
+    printf("Kernel driver: adding %d + %d\n", a, b);
+    fflush(stdout);
+    return a + b;
+}
+
